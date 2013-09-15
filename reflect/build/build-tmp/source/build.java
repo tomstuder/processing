c24a106eaa37b739be1cb290e6182514e9c8a68a
Tom Studer <tom@tomstuder.com>
2013-09-15 19:52:12 -0400

    trying to reflect an image

---
+++ reflect/build/build-tmp/source/build.java
@@ -0,0 +1,41 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+
+PImage img;
+
+
+public void setup(){
+size(512, 512, P3D);
+img = loadImage("argue.png");
+
+
+image(img, 0, 0);
+
+rotate(0, 45, 0, 0);
+image(img, 50, 0);
+
+	
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ reflect/build/build.pde
@@ -0,0 +1,16 @@
+
+PImage img;
+
+
+void setup(){
+size(512, 512, P3D);
+img = loadImage("argue.png");
+
+
+image(img, 0, 0);
+
+rotate(0, 0, 0, 0);
+image(img, 50, 0);
+
+	
+}
