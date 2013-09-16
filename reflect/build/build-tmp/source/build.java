f2451074017f733d2aed6066a18521b31bea6294
Tom Studer <tom@tomstuder.com>
2013-09-15 20:25:51 -0400

    PShape, SVG, Scale to reflect, BOOOM

--- reflect/build/build-tmp/source/build.java
+++ reflect/build/build-tmp/source/build.java
@@ -15,25 +15,25 @@ import java.io.IOException;
 public class build extends PApplet {
 
 
-PImage img;
+PShape s;
 
 public void setup(){
 size(394, 500, P3D);
-img = loadImage("argue.png");
+s = loadShape("test.svg");
 
 pushMatrix();
 translate(0, 0);
-image(img, 0, 0);
+shape(s, 0, 0);
 popMatrix();
 
 // flippin images!!!
 pushMatrix();
 translate(394, 0);
 scale(-1,1);
-image(img, 0, 0);
+shape(s, 0, 0);
 popMatrix();
 
-saveFrame("saveImage/flippin_image.png");
+saveFrame("saveImage/flippin_image2.png");
 	
 }
   static public void main(String[] passedArgs) {
--- reflect/build/build.pde
+++ reflect/build/build.pde
@@ -1,22 +1,22 @@
 
-PImage img;
+PShape s;
 
 void setup(){
 size(394, 500, P3D);
-img = loadImage("argue.png");
+s = loadShape("test.svg");
 
 pushMatrix();
 translate(0, 0);
-image(img, 0, 0);
+shape(s, 0, 0);
 popMatrix();
 
 // flippin images!!!
 pushMatrix();
 translate(394, 0);
 scale(-1,1);
-image(img, 0, 0);
+shape(s, 0, 0);
 popMatrix();
 
-saveFrame("saveImage/flippin_image.png");
+saveFrame("saveImage/flippin_image2.png");
 	
 }
