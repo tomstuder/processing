619470ef92b64bf48894e5e56f3520ade738b33e
Tom Studer <tom@tomstuder.com>
2013-09-22 08:59:54 -0400

    Frame Rate Setup

---
+++ println/build/build-tmp/source/build.java
@@ -0,0 +1,40 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+public void setup(){
+  
+}
+
+public void draw(){
+// Set Frame rate, default is 60fps
+	frameRate(1);
+  // Understand how the draw function works with println("");
+
+  // Draw to the console
+  println("I'm Drawing");
+
+  // Count in the Console
+  println(frameCount);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ println/build/build.pde
@@ -0,0 +1,15 @@
+void setup(){
+  
+}
+
+void draw(){
+// Set Frame rate, default is 60fps
+	frameRate(1);  // sets framerate 1fps
+  // Understand how the draw function works with println("");
+
+  // Draw to the console
+  println("I'm Drawing");
+
+  // Count in the Console
+  println(frameCount);
+}
