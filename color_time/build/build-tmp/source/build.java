8f3fde3e9ce9615f8226093356dad0107844f416
tstuderTOPIC <tstuder@topicdesign.com>
2013-10-15 09:54:03 -0400

    color by time

---
+++ color_time/build/build-tmp/source/build.java
@@ -0,0 +1,43 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+int s = seconds();
+int m = minute();
+int h = hour();
+
+String now = "time:"+ h + m + s;
+
+public void setup(){
+
+	size(512, 512);
+
+}
+
+public void draw(){
+	// background(now);
+	println(now);
+
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ color_time/build/build.pde
@@ -0,0 +1,23 @@
+int s = second();
+int m = minute();
+int h = hour();
+
+String now = "time:"+ h + m + s;
+int now1 = h "+" m "+" s;
+
+
+void setup(){
+
+	size(512, 512);
+
+}
+
+void draw(){
+	// background(now);
+	println(now);
+
+
+}
+
+// Comments
+// Consider join()
