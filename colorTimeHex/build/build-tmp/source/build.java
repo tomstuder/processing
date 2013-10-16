c4ed610af7b1faca5c0a0f2d5c1b2db18b29470f
tstuderTOPIC <tstuder@topicdesign.com>
2013-10-15 23:42:14 -0400

    parsed int to create rgb value

---
+++ colorTimeHex/build/build-tmp/source/build.java
@@ -0,0 +1,46 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+public void setup(){
+	size(600, 600);
+	smooth();
+
+
+
+}
+
+public void draw(){
+
+int h = hour();
+int m = minute();
+int s = second();
+
+String t = "" + h + m + s;
+
+int n = PApplet.parseInt(t);	
+println(n);
+background(n);
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
