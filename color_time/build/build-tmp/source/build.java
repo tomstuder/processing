3c1189fbc61f84b0ea2d9f529c4cc2ef9378c9bc
tstuderTOPIC <tstuder@topicdesign.com>
2013-10-15 23:14:57 -0400

    hour min sec used as rgb values

--- color_time/build/build-tmp/source/build.java
+++ color_time/build/build-tmp/source/build.java
@@ -14,29 +14,32 @@ import java.io.IOException;
 
 public class build extends PApplet {
 
-int s = second();
-int m = minute();
-int h = hour();
-
-String now = "time:"+ h + m + s;
-// int now1 = h "+" m "+" s;
-
-
 public void setup(){
-
-	size(512, 512);
-
+size(500, 500);
 }
 
 public void draw(){
-	// background(now);
-	println(now);
 
+int s = second();
+int m = minute(); 
+int h = hour();
 
+String col = "colors " + h + m + s;
+
+background(s + 150,m + 100, h);
+
+for (int x = 0; x <=width; x+=20){
+	for (int y = 0; y <=height; y+=20){
+		// stroke(s,h,m);
+		fill(s+160, m+110, h, random(0, 200));
+		stroke(s+100, 0, 0,random(0, 200));
+		// strokeWeight(random(1));
+		rect(x, y, random(1, 20), random(1, 20));
+  }
+ }
+ // Use MovieMaker in Processing Tools to create .mov file (uncomment saveFrame below)
+ // saveFrame("render/line-######.tif");
 }
-
-// Comments
-// Consider join()
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "build" };
     if (passedArgs != null) {
--- color_time/build/build.pde
+++ color_time/build/build.pde
@@ -1,24 +1,26 @@
-int s = second();
-int m = minute();
-int h = hour();
-
-String now = "time:"+ h + m + s;
-// int now1 = h "+" m "+" s;
-
-
 void setup(){
-
-	size(512, 512);
-
+size(500, 500);
 }
 
 void draw(){
-	// background(now);
-	println(now);
 
+int s = second();
+int m = minute(); 
+int h = hour();
 
+String col = "colors " + h + m + s;
+
+background(s + 150,m + 100, h);
+
+for (int x = 0; x <=width; x+=20){
+	for (int y = 0; y <=height; y+=20){
+		// stroke(s,h,m);
+		fill(s+160, m+110, h, random(0, 200));
+		stroke(s+100, 0, 0,random(0, 200));
+		// strokeWeight(random(1));
+		rect(x, y, random(1, 20), random(1, 20));
+  }
+ }
+ // Use MovieMaker in Processing Tools to create .mov file (uncomment saveFrame below)
+ // saveFrame("render/line-######.tif");
 }
-
-// Comments
-// Consider join()
-// Integer.parseInt(string); 
